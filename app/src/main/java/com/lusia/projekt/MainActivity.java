2e11bf2fe41161087aabf06b0656ab09da954d9e
KoffeCraft <lucycraft.minecrafte@gmail.com>
2026-04-17 12:52:06 +0200

    Logic: Pełna implementacja wzoru BMI i interpretacji wyniku

--- app/src/main/java/com/lusia/projekt/MainActivity.java
+++ app/src/main/java/com/lusia/projekt/MainActivity.java
@@ -41,15 +41,33 @@ public class MainActivity extends AppCompatActivity {
         String wagaStr = etWaga.getText().toString();
         String wzrostStr = etWzrost.getText().toString();
 
-        // Sprawdzenie czy pola nie są puste (podstawa programowa: instrukcja warunkowa)
         if (!wagaStr.isEmpty() && !wzrostStr.isEmpty()) {
             float wagaWartosc = Float.parseFloat(wagaStr);
             float wzrostWartosc = Float.parseFloat(wzrostStr);
 
-            // Tutaj w następnym kroku dodamy matematykę i wyświetlanie
-            tvWynik.setText("Dane pobrane: " + wagaWartosc + " kg");
+            // 1. Obliczenie BMI (wzór: waga / wzrost_w_metrach^2)
+            float wzrostMetry = wzrostWartosc / 100;
+            float bmi = wagaWartosc / (wzrostMetry * wzrostMetry);
+
+            // 2. Interpretacja wyniku przy użyciu drabinki if-else if
+            String kategoria;
+            if (bmi < 18.5) {
+                kategoria = "Niedowaga";
+            } else if (bmi >= 18.5 && bmi < 24.9) {
+                kategoria = "Waga prawidłowa";
+            } else if (bmi >= 25 && bmi < 29.9) {
+                kategoria = "Nadwaga";
+            } else {
+                kategoria = "Otyłość";
+            }
+
+            // 3. Wyświetlenie sformatowanego wyniku
+            // %.2f oznacza zaokrąglenie do 2 miejsc po przecinku
+            tvWynik.setText(String.format("Twoje BMI: %.2f\nStatus: %s", bmi, kategoria));
+
         } else {
-            tvWynik.setText("Błąd: Wypełnij wszystkie pola!");
+            // Mały bonus: Toast to krótka wiadomość systemowa na dole ekranu
+            android.widget.Toast.makeText(this, "Wprowadź wszystkie dane!", android.widget.Toast.LENGTH_SHORT).show();
         }
     }
 }
