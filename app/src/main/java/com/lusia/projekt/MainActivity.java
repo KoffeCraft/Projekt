a91b79f1d4d65ec88fe6bf0dc9db714fa0cfeb78
KoffeCraft <lucycraft.minecrafte@gmail.com>
2026-04-17 12:50:22 +0200

    Logic: Inicjalizacja zmiennych i powiązanie z widokiem (findViewById)

--- app/src/main/java/com/lusia/projekt/MainActivity.java
+++ app/src/main/java/com/lusia/projekt/MainActivity.java
@@ -1,24 +1,32 @@
 package com.lusia.projekt;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.lusia.projekt.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 1. Deklaracja zmiennych dla elementów UI
+    private EditText etWaga;
+    private EditText etWzrost;
+    private Button btnOblicz;
+    private TextView tvWynik;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // 2. Inicjalizacja zmiennych - powiązanie z ID z pliku XML
+        etWaga = findViewById(R.id.etWaga);
+        etWzrost = findViewById(R.id.etWzrost);
+        btnOblicz = findViewById(R.id.btnOblicz);
+        tvWynik = findViewById(R.id.tvWynik);
+
+        // Tutaj w następnym kroku dodamy obsługę kliknięcia
     }
 }
